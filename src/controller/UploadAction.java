062dee51f4778e57dad5f43b1e59aa2574db3a9b
zadbee <danielzhangbo@gmail.ocm>
2014-06-05 16:50:22 -0400

    Bug fixed

--- src/controller/UploadAction.java
+++ src/controller/UploadAction.java
@@ -2,7 +2,6 @@ package controller;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import javax.servlet.ServletException;
