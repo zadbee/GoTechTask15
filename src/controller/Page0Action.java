329b60988c61e4bac62218ee83c142820fb70d5b
MengdiSun <mengdi.sun90@gmail.com>
2014-06-09 00:18:57 -0400

    Mengdi

--- src/controller/Page0Action.java
+++ src/controller/Page0Action.java
@@ -34,22 +34,61 @@ public class Page0Action extends Action {
 		
 		String[] optout = request.getParameterValues("opt-out");
 		for (String s : optout) {
-			if (s.equals("1"))
+			if (s.equals("1")) {
 				q.opt_phone = true;
-			if (s.equals("2"))
-				q.opt_website = true;
-			if (s.equals("4"))
-				q.opt_mail = true;
-			if (s.equals("8"))
-				q.opt_cookie = true;
-			if (s.equals("16"))
-				q.opt_doNotTrack = true;
+			
+				
+			}
+			if (s.equals("2")) {
+		
+			q.opt_website = true;
+			
+			}
+			if (s.equals("4")) {
+			
+			q.opt_mail = true;
+			 
+			}
+			if (s.equals("8")) {
+		
+			q.opt_cookie = true;
+			
+			}
+			if (s.equals("16")) {
+		
+			q.opt_doNotTrack = true;
+			}
 		}															// opt_out_option
-		session.setAttribute("opt_phone", q.opt_phone);
-		session.setAttribute("opt_website", q.opt_website);
-		session.setAttribute("opt_mail", q.opt_mail);
-		session.setAttribute("opt_cookie", q.opt_cookie);
-		session.setAttribute("opt_doNotTrack", q.opt_doNotTrack);
+		if (q.opt_phone == true) {
+			session.setAttribute("opt_phone", q.opt_phone);
+		}
+		else {
+		session.setAttribute("opt_phone", null);
+		}
+		if (q.opt_website == true) {
+			session.setAttribute("opt_website", q.opt_website);
+		}
+		else {
+			session.setAttribute("opt_website", null);
+		}
+		if (q.opt_mail == true) {
+			session.setAttribute("opt_mail", q.opt_mail);
+		}
+		else {
+			session.setAttribute("opt_mail", null);
+		}
+		if (q.opt_cookie == true) {
+			session.setAttribute("opt_cookie", q.opt_cookie);
+		}
+		else {
+			session.setAttribute("opt_cookie", null);
+		}
+		if (q.opt_doNotTrack == true) {
+			session.setAttribute("opt_doNotTrack", q.opt_doNotTrack);
+		}
+		else {
+			session.setAttribute("opt_doNotTrack", null);
+		}
 		
 		if (request.getParameter("optionsRadio").equals("share")){
 			q.hasAffiliates = true;									// hasAffiliates
